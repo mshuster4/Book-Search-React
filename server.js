d956a57902fbc4929123a8dfb3be311d0baf52ba
Emroze <emrozesd@users.noreply.github.com>
2019-04-24 20:32:01 -0700

    Wrote logic for google books api

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
